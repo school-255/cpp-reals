c91125b45c0846340c841d43bae820657b13eb79
Arseniy <yarmolinskiyam@gmail.com>
2023-11-24 09:44:10 +0000

    tria

---
+++ triangle_area/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, S;
+
+    std::cin >> a >> b >> c;
+
+    //!v Начало решения
+
+    // S = ?
+
+    //!^ Конец решения
+
+    std::cout << S;
+
+    return 0;
+}
